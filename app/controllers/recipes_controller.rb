3d675e050fd02ae053df15a9690830464a3e8267
gofar09 <gordon.d.farrell@gmail.com>
2018-08-09 11:22:05 -0400

    Generate new table for recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,51 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
+  # GET /recipes
+  def index
+    @recipes = Recipe.all
+
+    render json: @recipes
+  end
+
+  # GET /recipes/1
+  def show
+    render json: @recipe
+  end
+
+  # POST /recipes
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      render json: @recipe, status: :created
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /recipes/1
+  def update
+    if @recipe.update(recipe_params)
+      render json: @recipe
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /recipes/1
+  def destroy
+    @recipe.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def recipe_params
+      params.require(:recipe).permit(:name, :alcoholic, :img, :glass, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7, :ingredient8, :ingredient9, :ingredient10, :ingredient11, :ingredient12, :ingredient13, :ingredient14, :ingredient15)
+    end
+end
---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,3 @@
+class RecipeSerializer < ActiveModel::Serializer
+  attributes :id, :name, :alcoholic, :img, :glass, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7, :ingredient8, :ingredient9, :ingredient10, :ingredient11, :ingredient12, :ingredient13, :ingredient14, :ingredient15
+end
